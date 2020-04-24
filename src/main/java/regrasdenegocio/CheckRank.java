438b3b365a5b23f1dbf632394fa7ff820116ed09
Mateus Vitor <stanniack@gmail.com>
2020-04-24 15:48:50 -0300

    Refatoração da classe CheckRank
    Excluindo e refatorando alguns códigos.

--- src/main/java/regrasdenegocio/CheckRank.java
+++ src/main/java/regrasdenegocio/CheckRank.java
@@ -36,77 +36,8 @@ public class CheckRank {
     private static final int LEVEL = 3;
     private static final int POINTS = 4;
 
+    /* Estem método é adaptado para rodar em qualquer circunstância, mas é recomendado que se use depois do discover ou updateCharacter*/
     public void checkGlobalRank() {
-
-        Long tempoInicial = System.currentTimeMillis();
-        List<String> listaDeElementos = new ArrayList<>();
-        List<CharacterRank> listaDeChars = new ArrayList<>();
-        List<String> mundos = MockWorldsTibia.getWorldsTibia();
-
-        for (int i = 0; i < 1; i++) {
-
-            Long tempoInicialPorMundo = System.currentTimeMillis();
-
-            for (int n = 1; n <= PROFESSION; n++) {
-                Long tempoInicialPorProfissao = System.currentTimeMillis();
-
-                for (int j = FIRST_PAGE; j <= LAST_PAGE; j++) {
-                    String url = "https://www.tibia.com/community/?subtopic=highscores&world="
-                            + mundos.get(i) + "&list=experience&profession=" + n + "&currentpage=" + j;
-
-                    try {
-
-                        Document arquivoHtml = Jsoup.connect(url).get();
-                        listaDeElementos = arquivoHtml.getElementsByTag("td").eachText();
-
-                        for (int k = CONTENT_START; k < listaDeElementos.size() - INCREMENTOR; k += 5) {
-
-                            // Tiras as vírgulas da exp
-                            String valorConvertido = listaDeElementos.get(k + POINTS).replace(",", "");
-                            Long valorCvtLong = Long.valueOf(valorConvertido);
-
-                            listaDeChars.add(new CharacterRank(
-                                    listaDeElementos.get(k),
-                                    listaDeElementos.get(k + NAME),
-                                    listaDeElementos.get(k + VOCATION),
-                                    listaDeElementos.get(k + LEVEL),
-                                    valorCvtLong,
-                                    mundos.get(i))
-                            );
-                        }
-
-                    } catch (IOException e) {
-                        System.out.println("Erro na execução." + e);
-
-                    } catch (NumberFormatException e) {
-                        System.out.println("Erro de conversão: " + e);
-                    }
-
-                } // for das páginas
-
-                Long tempoFinalPorProfissao = System.currentTimeMillis();
-                System.out.println("A profissão " + n + " foi minerada com "
-                        + ((tempoFinalPorProfissao - tempoInicialPorProfissao) / 1000) + " segundos.");
-
-            } // for das profissões
-
-            Long tempoFinalPorMundo = System.currentTimeMillis();
-            System.out.println("O servidor de " + mundos.get(i) + " gastou " + ((tempoFinalPorMundo - tempoInicialPorMundo) / 1000)
-                    + " segundos para ser minerado");
-
-        } // for dos mundos
-
-        Long tempoFinal = System.currentTimeMillis();
-        System.out.println("O tempo total para minerar todos os servidores foi de " + ((tempoFinal - tempoInicial) / 1000)
-                + " segundos");
-
-        /* Exibir conteúdo */
-        this.exibeConteudo(listaDeChars);
-
-    }// fim do metodo
-
-    /* Esse código rodará ...*/
-    public void discoverIsRanked() {
 
         Long startTime = System.currentTimeMillis();
         List<String> worlds = MockWorldsTibia.getWorldsTibia();
@@ -375,19 +306,6 @@ public class CheckRank {
         System.out.println("O tempo total para minerar todos os skills, servidores e profissões foram de: "
                 + ((finalTime - startTime) / 1000) + " segundos.");
 
-    }
-
-    /* Eliminar este método futuramente */
-    private void exibeConteudo(List<CharacterRank> lista) {
-
-        System.out.println("___________________________________________________");
-        Collections.sort(lista);
-
-        for (int i = 0; i < lista.size(); i++) {
-            System.out.println((i + 1) + " " + lista.get(i).getName() + " " + lista.get(i).getLevel() + " "
-                    + lista.get(i).getWorld());
-
-        }
     }
 
 }
