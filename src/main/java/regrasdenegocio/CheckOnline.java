daa3c0e34f630387ceab29f2e53bbc161726ab28
Mateus Vitor <stanniack@gmail.com>
2020-05-16 16:39:39 -0300

    CheckOnline: Eliminação de código inutilizável

--- src/main/java/regrasdenegocio/CheckOnline.java
+++ src/main/java/regrasdenegocio/CheckOnline.java
@@ -1,19 +1,13 @@
 package regrasdenegocio;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import utils.WorldsTibiaUtil;
 
 public class CheckOnline {
 
-    private List<String> listCharacters = new ArrayList<>();
 
     /* Elemento da lista que contém os players online */
     private final int PLAYERS_CONTENT = 2;
@@ -22,93 +16,10 @@ public class CheckOnline {
     /* Eliminação de elemtentos na lista indesejáveis */
     private final int PLAYER_INCREMENTOR = 3;
 
-    public synchronized void checkOnline() {
-        int novos;
-        List<String> worlds = WorldsTibiaUtil.getWorldsTibia();
-
-        while (true) {
-
-            for (int i = 0; i < worlds.size(); i++) {
-                String url = "https://www.tibia.com/community/?subtopic=worlds&world=" + worlds.get(i);
-                novos = 0;
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    System.out.println("Erro na thread" + e);
-                }
-
-                try {
-
-                    Document arquivoHtml = Jsoup.connect(url).get();
-
-                    Elements elementosODD = arquivoHtml.getElementsByClass("Odd");
-                    Elements elementosEven = arquivoHtml.getElementsByClass("Even");
-
-                    for (Element elemento : elementosODD) {
-                        String text = elemento.getElementsByTag("tr").text();
-
-                        /* Remove o level e a vocação da string */
-                        text = text.replaceAll("[0-9]{3}", "");
-                        text = text.replaceAll("[0-9]{2}", "");
-                        text = text.replaceAll("[0-9]{1}", "");
-                        text = text.replace("Elite Knight", "");
-                        text = text.replace("Elder Druid", "");
-                        text = text.replace("Master Sorcerer", "");
-                        text = text.replace("Royal Paladin", "");
-                        text = text.replace("Knight", "");
-                        text = text.replace("Druid", "");
-                        text = text.replace("Sorcerer", "");
-                        text = text.replace("paladin", "");
-
-                        /* Adiciona na lista o resultado*/
-                        if (!listCharacters.toString().contains(text)) {
-                            listCharacters.add(text);
-                            novos++;
-                        }
-
-                    }
-
-                    for (Element elemento : elementosEven) {
-                        String text = elemento.getElementsByTag("tr").text();
-
-                        /* Remove o level e a vocação da string */
-                        text = text.replaceAll("[0-9]{3}", "");
-                        text = text.replaceAll("[0-9]{2}", "");
-                        text = text.replaceAll("[0-9]{1}", "");
-                        text = text.replace("Elite Knight", "");
-                        text = text.replace("Elder Druid", "");
-                        text = text.replace("Master Sorcerer", "");
-                        text = text.replace("Royal Paladin", "");
-                        text = text.replace("Knight", "");
-                        text = text.replace("Druid", "");
-                        text = text.replace("Sorcerer", "");
-                        text = text.replace("paladin", "");
-
-                        /* Adiciona na lista o resultado*/
-                        if (!listCharacters.toString().contains(text)) {
-                            listCharacters.add(text);
-                            novos++;
-                        }
-
-                    }
-
-                    if (novos != 0) {
-                        System.out.println(novos + " novos personagens.");
-                        /* Regra de persistência aqui */
-                    }
-
-                    System.out.println("Servidor: " + worlds.get(i));
-
-                } catch (IOException e) {
-                    System.out.println("Erro na execução." + e);
-                }
-
-            } // fim for worlds
-
-        }
-
-    }
+    /* Atributos */
+    private final int NAME = 0;
+    private final int LEVEL = 1;
+    private final int PROFESSION = 2;
 
     public void getOnlinePlayers() {
         String url = "https://www.tibia.com/community/?subtopic=worlds&world=" + "Antica";
@@ -119,7 +30,14 @@ public class CheckOnline {
             Elements chosenElements = contentElements.getElementsByClass("InnerTableContainer");
 
             List<String> elementsList = chosenElements.get(PLAYERS_CONTENT).getElementsByTag("td").eachText();
-            System.out.println(elementsList.toString());
+            int z = 0;
+
+            for (int i = 0; i < elementsList.size(); i += PLAYER_INCREMENTOR) {
+                System.out.println(elementsList.get(i));
+                z++;
+            }
+
+            System.out.println(z);
 
         } catch (IOException ex) {
         }
