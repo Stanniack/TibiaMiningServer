efe6fd563018ac58fac9d96fd5c20f2feebd30c2
Mateus Vitor <stanniack@gmail.com>
2020-05-26 06:37:45 -0300

    Adicionando testes com arquivos para debug em CheckCharacter
    Classe já exisita mas foi deletada sem querer.

--- src/main/java/tibiamining/TestesComArquivos.java
+++ src/main/java/tibiamining/TestesComArquivos.java
@@ -1,16 +1,38 @@
 package tibiamining;
 
+import businessrules.CheckCharacter;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import businessrules.CheckCharacter;
 
 public class TestesComArquivos {
 
     public static void main(String[] args) {
+        List<String> listaItens = new ArrayList<>();
 
+        try {
+
+            /* Alterar  */
+            String file = "C:\\Users\\fiodo\\OneDrive\\Área de Trabalho\\tmteste.txt";
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String[] itens = null;
+
+            while (br.ready()) {
+
+                itens = br.readLine().split("&");
+                listaItens.add(itens[0]);
+
+            }
+
+            new CheckCharacter().updateCharacter("Crimsix Tanagaiola", listaItens);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Arquivo não encontrado: " + ex);
+        } catch (IOException ex) {
+            System.out.println("Falha I/O - E/S: " + ex);
+        }
     }
 }
